02f7fde87814ca166bc86c5cf68cf3667467ed56
@analratnagaikwad <analratnagaikwad@gmail.com>
2024-05-24 21:24:36 +0530

    test2

--- lambda_function.py
+++ lambda_function.py
@@ -1,6 +1,7 @@
+from lib2to3.pgen2.pgen import DFAState
 import pandas as pd
 def lambda_handler(event, context): 
     d = {'col1': [1,2], 'col2': [3,4]} 
     df = pd.DataFrame(data=d)
-print(df)
+print(DFAState)
 print('Done x1.1')
